098482eb95ab244c6af7768b7cf3f9b50ab0dc9f
Solonichniy Volodimir <vsolo0@gmail.com>
2019-11-02 18:56:37 +0200

    v3

---
+++ src/main/java/DBPostgres.java
@@ -0,0 +1,96 @@
+import java.sql.*;
+
+public class DBPostgres {
+    static private Connection connection = null;
+    static private DBPostgres dbPostgres = null;
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/cashflow";
+    static final String USER = "cashflow";
+    static final String PASS = "vbwqu1pa";
+
+    static public DBPostgres create() {
+        if (dbPostgres == null) {
+            dbPostgres = new DBPostgres();
+            dbPostgres.connect();
+        }
+        return dbPostgres;
+    }
+
+    public void connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You successfully connected to database now");
+        } else {
+            System.out.println("Failed to make connection to database");
+        }
+    }
+
+    public boolean isConnection() {
+        return connection != null;
+    }
+
+    public void close() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void execute(String sqlQuery) {
+        if (connection == null) {
+            System.out.println("Connection is down!");
+            return;
+        }
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            statement.execute(sqlQuery);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void getInfo() throws SQLException {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT VERSION()");
+        if (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+    }
+
+    public ResultSet executeQuery(String sqlQuery){
+        PreparedStatement pst = null;
+        try {
+            pst = connection.prepareStatement(sqlQuery);
+            return pst.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,31 +2,22 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        FPostgres fPostgres = new FPostgres();
-        fPostgres.connect();
-        if (!fPostgres.isConnection()) {
+        DataBase DataBase = new DataBase();
+        if (!DataBase.isConnection()) {
             return;
         }
 
-        //fPostgres.createTable();
-        //fPostgres.createEmptyTable();
-        //fPostgres.dropAllTable();
+        DataBase.createEmptyTable();
+        //DataBase.dropAllTable();
 
-
-//        for (int i = 10; i < 100000; i++) {
-//            fPostgres.insertAuthor(i,"Author " + i);
-//        }
-
-//        for (int i = 0; i < 10; i++) { //55s
-//            for (int j = 0; j < 100; j++) {
-//                int id = i*100+j+10;
-//                fPostgres.insertAuthorAdd(id,"Author " + id);
-//            }
-//            fPostgres.insertAuthorPush();
-//        }
-
-
-        fPostgres.printAuthors();
-        fPostgres.close();
+        for (int i = 0; i < 1000; i++) { //55s
+            for (int j = 0; j < 100; j++) {
+                int id = i*100+j;
+                DataBase.insertAuthorAdd(id,"Author " + id);
+            }
+            DataBase.insertAuthorPush();
+        }
+        DataBase.printAuthors();
+        DataBase.close();
     }
 }
