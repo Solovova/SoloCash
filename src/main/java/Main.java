03bf2c7ae654946809cb385e2d38a9c27c2fb319
Solonichniy Volodimir <vsolo0@gmail.com>
2019-11-02 15:03:50 +0200

    2 type insert in database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws SQLException {
+        FPostgres fPostgres = new FPostgres();
+        fPostgres.connect();
+        if (!fPostgres.isConnection()) {
+            return;
+        }
+
+        fPostgres.createTable();
+
+//        for (int i = 10; i < 100000; i++) {
+//            fPostgres.insertAuthor(i,"Author " + i);
+//        }
+
+        for (int i = 0; i < 10000; i++) { //55s
+            for (int j = 0; j < 100; j++) {
+                int id = i*100+j+10;
+                fPostgres.insertAuthorAdd(id,"Author " + id);
+            }
+            fPostgres.insertAuthorPush();
+        }
+
+
+        //fPostgres.printAuthors();
+        fPostgres.close();
+
+
     }
 }
