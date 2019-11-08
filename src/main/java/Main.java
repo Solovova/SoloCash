2658a9997f5f5bdaa1c6b8ce9a9aedb68c0689f8
Solonichniy Volodimir <vsolo0@gmail.com>
2019-11-08 18:52:30 +0200

    v32 wss

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import wss.WSS;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main( String[] args ) throws InterruptedException , IOException {
+        WSS s = new WSS( 8887 );
+        s.start();
+        System.out.println( "ChatServer started on port: " + s.getPort() );
+
+        BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
+        while ( true ) {
+            String in = sysin.readLine();
+            s.broadcast( in );
+            if( in.equals( "exit" ) ) {
+                s.stop(1000);
+                break;
+            }
+        }
     }
 }
